e6b7d9d4cdd8560c8c011f4781bd937887f0b7a3
ELDADBRHANO <ebrhano@gmail.com>
2022-05-23 13:08:01 +0300

    class_commit

---
+++ main.js
@@ -0,0 +1,114 @@
+// // 1
+// function getDateFrom(params) {
+  
+
+// var myDate = new Date()
+// console.log(myDate)
+
+// }
+// getDateFrom()
+
+// 2
+// function getDateAsString(dateString) {
+//   dateString = new Date(dateString)
+//   console.log(dateString)
+// }
+// getDateAsString("5/13/99")
+
+
+
+// 3
+// function getDateFromUser() {
+//  var userDate=new Date(prompt("enter date"))
+//  console.log(userDate)
+// }
+// getDateFromUser()
+
+
+
+
+// 4
+// function getDayReturnMonth() {
+//  var userDate = new Date(prompt())
+//   return userDate.getMonth()
+// }
+// console.log(getDayReturnMonth())
+
+
+
+// 5
+// function getHours() {
+//   var getHourUser= new Date(prompt("enter date"))
+//   return getHourUser.getFullYear()
+// }
+// console.log(getHours())
+
+
+
+// 6
+// function getMints() {
+//   var mints = new Date(prompt("enter date"))
+//   return mints.getMinutes()
+
+// }
+// console.log(getMints())
+
+
+// 7
+// function GetTwoDatesFromUser() {
+//   var get=new Date(prompt("enter date"))
+//   var get1=new Date(prompt("enter date"))
+//   if(get>get1){
+//     return get
+//   }else{
+//     return get1
+//   }
+// }
+// console.log(GetTwoDatesFromUser())
+
+
+
+
+// 8
+// function getUserAge() {
+//   var UserAge=new Date(prompt("enter now date"));
+//   var birthDay= new Date(prompt("enter your birthday"));
+//   var age =UserAge.getFullYear() - birthDay.getFullYear(); 
+//   return age
+// }
+// console.log(getUserAge());
+
+
+
+
+// 9
+// function getDateAsString() {
+//  var getUserDate=new Date(prompt("enter date in format of MM-DD-YYYY"))
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 10
+// function getRndNum(){
+//   var rndMonth= Math.floor(Math.random()*12);
+//   var rndDay = Math.floor(Math.random()*(31-1)+1);
+//   var dateStr= rndMonth+"/"+rndDay+"/1996";
+//   var date = new Date(dateStr);
+//   return date;
+// }
+// console.log(getRndNum());
